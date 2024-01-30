6d961e5208c62581290eb427caa74f7949bec129
Cristian Meneses <menesesc@gmail.com>
2024-01-29 23:59:34 -0300

    arreglo map

--- app/ui/search/table.tsx
+++ app/ui/search/table.tsx
@@ -11,29 +11,30 @@ export default async function InfoTable({
 }) {
   if (query.length > 2) { 
     const documents = await fetchFilteredInfo(query, currentPage);
-    if (documents.length > 0) {
-        return (
-            <div className="mt-6 flow-root">
-                    {documents?.map((doc) => (
-                    <div
-                        key={doc._id}
-                        className="mb-2 w-full rounded-md bg-white"
-                    >
-                        <Card 
-                        title={doc.title} 
-                        description={doc.description}
-                        data={doc.data} 
-                        votes= {doc.votes} 
-                        category={(doc.category==="ERRORES"?"errores":"partes")}
-                        tags={String(doc.tags)}/>
-                    </div>
-                    ))}
-            </div>
-        )
-    } else {
-        return (
-            <h1>No se encontro nada</h1>
-        )
-    }
-    }
+    if (Array.isArray(documents)) {
+        if (documents.length > 0) {
+            return (
+                <div className="mt-6 flow-root">
+                        {documents?.map((doc) => (
+                        <div
+                            key={doc._id}
+                            className="mb-2 w-full rounded-md bg-white"
+                        >
+                            <Card 
+                            title={doc.title} 
+                            description={doc.description}
+                            data={doc.data} 
+                            votes= {doc.votes} 
+                            category={(doc.category==="ERRORES"?"errores":"partes")}
+                            tags={String(doc.tags)}/>
+                        </div>
+                        ))}
+                </div>
+            )
+        } else {
+            return (
+                <h1>No se encontro nada</h1>
+            )
+        }
+    }}
 }
