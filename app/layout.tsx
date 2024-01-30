6141f9fedb541a94b4b811a7a2c2094375b94c3c
Cristian Meneses <menesesc@gmail.com>
2024-01-30 18:09:23 -0300

    pwa update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,16 @@ import TopNav from './ui/nav/topnav';
 import { ClerkProvider } from '@clerk/nextjs'
 import { Analytics } from '@vercel/analytics/react';
 
+export const metadata = {
+  title:"ProCash",
+  description:"Herramientas para tecnicos de Prosegur Cash",
+  manifest: "/manifest.json",
+  icons: {
+    apple: "/icon.png"
+  },
+  themecolor: "#000000",
+}
+
 export default function RootLayout({
   children,
 }: {
